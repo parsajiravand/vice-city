f15abe2940c807bbb806a4f9d6117a68355c6b3f
parsajiravand <parsajiravand@gmail.com>
2023-06-03 15:31:28 -0700

    compelte labs section

--- src/components/Labs.vue
+++ src/components/Labs.vue
@@ -31,6 +31,36 @@
       </div>
       <div class="col-6 d-none d-md-flex">
         <img src="@/assets/labs.png" alt="labs" style="max-width: 600.72px" />
+        <img
+          src="@/assets/header-main-vector-4.svg"
+          class="position-absolute"
+          alt="vector"
+        />
+      </div>
+    </div>
+    <div class="row text-center justify-content-center my-6">
+      <div class="col-md-7 col-12 d-flex justify-content-center flex-column">
+        <h4 class="fw-bold">
+          Vice City Labs is mainly focused on early-stage startups providing
+          technological solutions through the usage of Artificial Intelligence
+          and Web 3 in ANY type of industry!
+        </h4>
+        <div class="h  mt-2">
+          <b>
+            If you think our incubation program brings the missing parts of the
+            puzzle you are looking to solve, Let's take the first step and
+            schedule a discovery call!
+          </b>
+          <span class="text-gray-500">
+            We may be one meeting away from starting the next BIG million-dollar
+            startup!
+          </span>
+        </div>
+        <div class="my-4">
+          <button class="btn btn-custom-primary text-uppercase px-4 fw-bold">
+            Schedule a Call
+          </button>
+        </div>
       </div>
     </div>
   </section>
