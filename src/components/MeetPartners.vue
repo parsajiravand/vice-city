3c460667c7974ce9d69b75fca367a01fc8fa9081
parsajiravand <parsajiravand@gmail.com>
2023-06-04 04:17:20 -0700

    Change meet partner structor

--- src/components/MeetPartners.vue
+++ src/components/MeetPartners.vue
@@ -1,82 +1,94 @@
-<script lang="ts" setup>
-const imageUrl = (name: string): string => {
-  return new URL(`../assets/${name}.png`, import.meta.url).href;
-};
-const partners: {
-  name: string;
-  img: string;
-}[] = [
-  {
-    name: "Lawrence Mansour",
-    img: "Lawrence-Mansour",
-  },
-
-  {
-    name: "Steven Harris",
-    img: "Steven-Harris",
-  },
-  {
-    name: "Belbron Emanoel",
-    img: "Belbron-Emanoel",
-  },
-];
-</script>
+<script lang="ts" setup></script>
 <template>
-  <section
-    class="meet-partners row px-4 px-md-0 container justify-content-center mx-auto"
-  >
-    <div class="row justify-content-center align-items-center">
-      <div class="col-md-6 col-12 px-4">
-        <h1 class="fw-bold">Meet Your New Partners</h1>
-        <p class="h5 py-4 w-75">
-          <span class="text-gray-500">
+  <section class="position-relative team-section pt-0 pt-md-5 pb-0 pb-md-5 z-2">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6">
+          <h2 class="display-5 fw-bold lh-sm">Meet Your New Partners</h2>
+          <p class="fs-5 fw-normal lh-base mb-3">
             Since 2015, Lawrence Mansour, Belbron Emanoel, and Steven Harris
             have been running EZ-AD and scaling several successful companies to
             more than a million dollars in annual recurring revenue. At its
             core, they are hard-core entrepreneurs capable of executing any new
             project, regardless of the scope.
-            <br />
-            <br />
+          </p>
+          <p class="fs-5 fw-normal lh-base mb-5 mb-md-0">
             Now, all that experience is being directed into Vice City Labs to
             incubate, build and scale the most memorable companies involved in
-            the usage of AI and Web 3.</span
-          >
-        </p>
-      </div>
-      <div class="col-6">
-        <div class="d-flex flex-column align-items-center">
-          <!-- show 3 items last items center  -->
-          <div class="row justify-content-center">
-            <div
-              class="col-md-4 col-12 my-3 mx-3 d-flex align-items-center"
-              v-for="(item, index) in partners"
-              :key="index"
-            >
-              <div class="text-center">
+            the usage of AI and Web 3.
+          </p>
+        </div>
+        <div class="col-md-6 position-relative">
+          <img
+            src="https://storage.googleapis.com/content.ezadtv.com/2022/12/28/63ac75fd379c5_teamTriangle.webp"
+            alt=""
+            class="position-absolute top-0 end-0 z-1 teamOverImage"
+          />
+          <div class="row justify-content-center align-items-center">
+            <div class="col-md-6 col-xxl-5">
+              <figure
+                class="position-relative teamImage rounded-3 overflow-hidden"
+              >
                 <img
-                  :src="imageUrl(item.img)"
-                  :alt="item.name"
-                  class="img-fluid rounded-3"
+                  src="@/assets/Lawrence-Mansour.png"
+                  alt=""
+                  class="mw-100 w-100"
                 />
-                <h5 class="fw-bold mt-3">{{ item.name }}</h5>
-              </div>
+                <figcaption class="position-absolute bottom-0">
+                  <h6 class="fs-5 lh-base fw-bold px-3 px-md-1 px-lg-3">
+                    Lawrence Mansour
+                  </h6>
+                </figcaption>
+              </figure>
+              <figure
+                class="position-relative teamImage rounded-3 overflow-hidden"
+              >
+                <img
+                  src="@/assets/Steven-Harris.png"
+                  alt=""
+                  class="mw-100 w-100"
+                />
+                <figcaption class="position-absolute bottom-0">
+                  <h6 class="fs-5 lh-base fw-bold px-3 px-md-1 px-lg-3">
+                    Steven Harris
+                  </h6>
+                </figcaption>
+              </figure>
+            </div>
+            <div class="col-md-6 col-xxl-5">
+              <figure
+                class="position-relative teamImage rounded-3 overflow-hidden"
+              >
+                <img
+                  src="@/assets/Belbron-Emanoel.png"
+                  alt=""
+                  class="mw-100 w-100"
+                />
+                <figcaption class="position-absolute bottom-0">
+                  <h6 class="fs-5 lh-base fw-bold px-3 px-md-1 px-lg-3">
+                    Belbron Emanoel
+                  </h6>
+                </figcaption>
+              </figure>
             </div>
           </div>
         </div>
-
-        <img
-          src="@/assets/header-main-vector-4.svg"
-          class="position-absolute"
-          alt="vector"
-        />
       </div>
     </div>
-    <div class="text-center">
-      <h1 class="fw-bold">Ready to meet us?</h1>
-      <div class="my-4">
-        <button class="btn btn-custom-primary text-uppercase px-4 fw-bold">
-          Schedule a Call
-        </button>
+  </section>
+  <!-- Meet US Section -->
+  <section class="position-relative call-section pt-md-5 pt-0 pb-0 pb-md-5 z-2">
+    <div class="container position-relative">
+      <div class="row justify-content-center py-5">
+        <div class="col-md-9 text-center justify-content-center">
+          <h2 class="display-5 lh-sm fw-bold mb-4">Ready to meet us?</h2>
+          <a
+            href="#contact"
+            class="btn btn-custom-primary py-3 px-4 fw-bold text-uppercase lh-1 mx-auto"
+          >
+            Schedule a Call
+          </a>
+        </div>
       </div>
     </div>
   </section>
